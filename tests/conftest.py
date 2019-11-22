7c95d6516900884f89ffdda0eecb9dc315c6cf69
Ro-ma <roman.aladyev@xena.exchange>
2019-11-22 19:55:09 +0300

    correct test data generation

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,8 @@ with open("../accounts.json", "r") as read_file:
         try:
             jresp = json.loads(response.text)
         except:
+
+            logging.error('response code is ' + str(response.status_code))
             logging.error('Cant parse json, incorrect response is ' + response.text + '. Login data is' + str(account['login_data']))
         sessions.append(
             {
@@ -36,31 +38,41 @@ with open("../accounts.json", "r") as read_file:
 # need to add sessio not for init
 #{'X-Auth-Nonce': jresp['nonce'], 'sid': response.cookies['sid']}
 
-
-# need to skip other fixtures and tests
 def pytest_generate_tests(metafunc):
-    if 'neg_or_pos_account' in metafunc.fixturenames:
+    # valid account data
+    if 'account' in metafunc.fixturenames:
         test_data = []
         for session in sessions:
             if any([i for i in ['all', metafunc.function.__name__] if i in session['scopes']]):
-                spot = ''
-                one_of_margin = ''
                 for acc in session['accounts']:
-                    test_data.append(
-                        # (session_data, account, expected_result)
-                        (session, str(acc['id']), "200")
-                    )
+                     test_data.append(
+                         (session, str(acc['id']))
+                                      )
+        metafunc.parametrize("session, account ", test_data, ids=[i[0]['login_data']['email'] + '_' + i[1] + '_'
+                                                                  for i in test_data])
+        return
+    # invalid account data for spot/margin endpoints
+    if 'negative_account' in metafunc.fixturenames:
+        test_data = []
+        for session in sessions:
+            if any([i for i in ['negative', metafunc.function.__name__] if i in session['scopes']]):
 
-                spot_account = [i['id'] for i in session['accounts'] if i['kind']=='Spot'][0]
+                spot_account = [i['id'] for i in session['accounts'] if i['kind'] == 'Spot'][0]
                 margin_account = [i['id'] for i in session['accounts'] if i['kind'] == 'Margin'][0]
 
-                for i in ['-1', '0', 'nil', '..', '9999999999999999', str(spot_account-10), str(margin_account - 10),
-                            str(spot_account+1000000), str(margin_account + 1000000000)]:
+                for negative_account in ['-1',
+                                         '0',
+                                         'nil',
+                                         '9999999999999999',
+                                         str(spot_account - 10),
+                                         str(margin_account - 10),
+                                         str(spot_account + 1000000),
+                                         str(margin_account + 1000000000)
+                                         ]:
                     test_data.append(
-                        # (session_data, account, expected_result)
-                        (session, i, "400")
+                        (session, negative_account)
                     )
-        metafunc.parametrize("session, neg_or_pos_account, expected result", test_data)
+        metafunc.parametrize("session, negative_account", test_data)
         return
 
     return
@@ -70,14 +82,6 @@ def init_tests():
     logging.info('Start')
     yield
     logging.info('End')
-
-@pytest.fixture(scope='session', autouse=True)
-def init_tests():
-    logging.info('Start')
-    yield
-    logging.info('End')
-
-
 
 class BaseTest:
     pass
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,15 +1,48 @@
-from tests.conftest import BaseTest, logging
+from tests.conftest import logging, trading_url
 from src.client import client
+import pytest
 
 
+class TestAccountPositive():
 
-class TestAccount():
+    def test_get_account(self, session, account):
+       response = client.get('/api/account/'+account, address=trading_url, headers={'X-Auth-Nonce': session['nonce']},
+                             cookies=session['cookies'])
+       logging.info(response.headers)
+       logging.info(response.text)
+       response.assert_2xx()
 
-    def test_get_account(self, session, neg_or_pos_account, expected_result):
-       logging.info(session, neg_or_pos_account, expected_result)
-       # response = client.get('/api/account/'+account, address=trading_url, headers={'X-Auth-Nonce': sessio['nonce']},
-       #                      cookies=sessio['cookies'])
-       # logging.info(response.headers)
-       # logging.info(response.text)
+    def test_get_balances(self, session, account):
+       response = client.get('/api/account/' + account + '/balances', address=trading_url, headers={'X-Auth-Nonce': session['nonce']},
+                             cookies=session['cookies'])
+       logging.info(response.headers)
+       logging.info(response.text)
+       response.assert_2xx()
+
+    # '\u0000'
+    @pytest.mark.parametrize('name',['\r\n',
+                                     'null',
+                                     '*[]its#20$символов!/',
+                                     '\u200b',
+                                     '$;--한글']
+                             )
+    def test_post_info(self, session, account, name):
+       data = {
+        'name': name
+       }
+       response = client.post('/api/account/' + account + '/info', address=trading_url, json=data, headers={'X-Auth-Nonce': session['nonce']},
+                             cookies=session['cookies'])
+       logging.info(response.headers)
+       logging.info(response.text)
+       response.assert_2xx()
+
+class TestAccountNegative():
+
+    def test_get_account(self, session, negative_account):
+       logging.info(str(session) + negative_account)
+       response = client.get('/api/account/'+negative_account, address=trading_url,
+                             headers={'X-Auth-Nonce': session['nonce']}, cookies=session['cookies'])
+       logging.info(response.headers)
+       logging.info(response.text)
+       logging.info(response.status_code)
        # response.assert_2xx()
-
